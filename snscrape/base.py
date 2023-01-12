b0fab9c13d66d109defb55ddce8fe8f9e7cbc1da
Siddharth Mathur <mathursiddharth2000@gmail.com>
2023-01-12 00:30:40 -0800

    Fix Twitter SSL issue

--- snscrape/base.py
+++ snscrape/base.py
@@ -8,10 +8,26 @@ import logging
 import requests
 import time
 import warnings
+import ssl
 
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.poolmanager import PoolManager
+from urllib3.util.ssl_ import create_urllib3_context
 
 logger = logging.getLogger(__name__)
 
+# see "openssl ciphers" command for cipher names
+CIPHERS = "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384"
+
+class TlsAdapter(HTTPAdapter):
+	def __init__(self, ssl_options=0, **kwargs):
+		self.ssl_options = ssl_options
+		super(TlsAdapter, self).__init__(**kwargs)
+
+	def init_poolmanager(self, *pool_args, **pool_kwargs):
+		ctx = create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+		self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
 
 class _DeprecatedProperty:
 	def __init__(self, name, repl, replStr):
@@ -143,6 +159,8 @@ class Scraper:
 		self._retries = retries
 		self._proxies = proxies
 		self._session = requests.Session()
+		adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)  # prioritize TLS 1.2
+		self._session.mount("https://", adapter)
 
 	@abc.abstractmethod
 	def get_items(self):
